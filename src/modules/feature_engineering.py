645db7314e5403b0dd2fc5606c34085e4874b2bb
jandus <jalejandro.adu@gmail.com>
2021-08-30 22:34:23 -0400

    Add comments to python code

--- src/modules/feature_engineering.py
+++ src/modules/feature_engineering.py
@@ -3,6 +3,19 @@ import numpy as np
 
 # Function to convert all strings to lower
 def clean_data(x):
+    '''
+    Convert all strings to lower case. 
+
+    Parameters
+    ----------
+    x : str
+        String.
+           
+    Returns
+    -------
+    string in lower case or empty string.. 
+
+    '''
     if isinstance(x, list):
         return [str.lower(i) for i in x]
     else:
@@ -14,6 +27,20 @@ def clean_data(x):
         
 # create a column with all words needed for the recommender 
 def create_soup(x):
+    '''
+    Joins all the words to create a soup of words. 
+
+    Parameters
+    ----------
+    x : str
+        Text information.
+           
+    Returns
+    -------
+    soup of words : str.
+        Words in a string. 
+
+    '''
     return ' '.join(x['ingredients']) + ' ' + ' '.join(x['tags']) + ' '  + x['description']
 
 # Add two new features ingredients_string and tags_strings - These will be used for the recommendations.
